533017e3be7c9e552338cc675d926f1784721527
petrovve <petroffve@mail.ru>
2024-12-07 17:30:24 +0300

    added functions

--- my_program.py
+++ my_program.py
@@ -1,8 +1,12 @@
+def my_super_function():
+    print('What an awesome print!')
+
 def main():
     print('My first git program')
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
+    my_super_function()
 
 
 if __name__ == '__main__':
